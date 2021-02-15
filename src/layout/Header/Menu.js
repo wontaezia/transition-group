f39bd81adaa33f10532a3aedd4d8ef1d9e2746c5
dx's fe <wontae1206@gmail.com>
2021-02-15 18:53:07 +0900

    modify: add styled components

--- src/layout/Header/Menu.js
+++ src/layout/Header/Menu.js
@@ -2,46 +2,71 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-function Menu({ routes }) {
+function Menu({ partners }) {
     return (
-        <div class="menu">
-            <ul class="menu__left">
-                <li>
-                    <span class="menu__bubble">96</span>
-                    <h3>eXperience</h3>
-                </li>
-                <li>
-                    <h3>About</h3>
-                </li>
-                <li>
-                    <h3>Contact</h3>
-                </li>
-                <li>
-                    <span class="menu__bubble">6</span>
-                    <h3>Hiring</h3>
-                </li>
-            </ul>
-            <div class="menu__right">
-                <div class="menu__news">
+        <Container>
+            <Left>
+                {categories.map(({ id, path, label, bubble }) => (
+                    <li key={id}>
+                        {bubble ? <Bubble>{bubble}</Bubble> : ''}
+                        <h3>{label}</h3>
+                    </li>
+                ))}
+            </Left>
+            <Right>
+                <div className="menu__news">
                     <a href="#">
                         <h3>News</h3>
                     </a>
                 </div>
-                {routes.map(({ id, path, label }) => (
+                {partners.map(({ id, path, label }) => (
                     <NavLink className="header__partners" strict exact to={path} key={id} activeClassName="nav--active">
                         {label}
                     </NavLink>
                 ))}
-                <div class="menu__object">
+                <div className="menu__object">
                     <a href="#">
                         <h3>Plus X Object</h3>
                         <span>Plus inspiration for your creative,</span>
                         <span>Plus object for your life.</span>
                     </a>
                 </div>
-            </div>
-        </div>
+            </Right>
+        </Container>
     );
 }
 
 export default Menu;
+
+const categories = [
+    {
+        id: 1,
+        path: '/',
+        label: 'eXperience',
+        bubble: '96',
+    },
+    {
+        id: 2,
+        path: '/',
+        label: 'About',
+    },
+    {
+        id: 3,
+        path: '/',
+        label: 'Contact',
+    },
+    {
+        id: 4,
+        path: '/',
+        label: 'Hiring',
+        bubble: '6',
+    },
+];
+
+const Container = styled.div``;
+
+const Left = styled.ul``;
+
+const Bubble = styled.span``;
+
+const Right = styled.div``;
--- src/layout/Header/index-2nd.js
+++ src/layout/Header/index-2nd.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import * as Nav from 'layout/Header/styles';
+import styled from 'styled-components';
 import Menu from 'layout/Header/Menu';
 
 function Header() {
     return (
-        <Nav.Container>
-            <Nav.Logo>
-                <Nav.Plus>
+        <Container>
+            <Logo>
+                <Plus>
                     <span />
                     <span />
-                </Nav.Plus>
-                <Nav.Ex>
+                </Plus>
+                <Ex>
                     <span />
                     <span />
-                </Nav.Ex>
-            </Nav.Logo>
-            {partners.map(({ id, path, label }) => (
+                </Ex>
+            </Logo>
+            {routes.map(({ id, path, label }) => (
                 <NavLink className="header__partners" strict exact to={path} key={id} activeClassName="nav--active">
                     {label}
                 </NavLink>
             ))}
-            <Nav.MenuButton>
+            <MenuButton>
                 <span className="line1" />
                 <span className="line2" />
-            </Nav.MenuButton>
-            <Menu partners={partners} />
-        </Nav.Container>
+            </MenuButton>
+            <Menu routes={routes} />
+        </Container>
     );
 }
 
 export default Header;
 
-const partners = [
+const routes = [
     {
         id: 1,
         path: '/union',
@@ -58,14 +58,20 @@ const partners = [
         path: '/union/cosmicray',
         label: 'Cosmicray',
     },
-    {
-        id: 6,
-        path: '/',
-        label: 'Home',
-    },
-    {
-        id: 7,
-        path: '/about',
-        label: 'About',
-    },
 ];
+
+export const Container = styled.nav`
+    ${({ theme }) => theme.flex('center', 'center')}
+
+    .nav--active {
+        color: #f00;
+    }
+`;
+
+export const Logo = styled.h1``;
+
+export const Plus = styled.div``;
+
+export const Ex = styled.div``;
+
+export const MenuButton = styled.button``;
